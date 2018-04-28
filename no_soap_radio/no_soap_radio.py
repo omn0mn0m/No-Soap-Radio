97d291d44011748b6741e2161c21603a4662f92c
Nam Tran <tranngocnam97@yahoo.com>
2018-04-28 15:57:50 -0400

    Font change

--- no_soap_radio/no_soap_radio.py
+++ no_soap_radio/no_soap_radio.py
@@ -86,50 +86,50 @@ def cleanup():
 # ================ Mode Selection ===============
 mode_frame = tk.Frame(root)
 
-mode_label = tk.Label(mode_frame, text="Mode", font=(None, 32)).grid(column=0, row=0, sticky='W')
+mode_label = tk.Label(mode_frame, text="Mode", font=(None, 30)).grid(column=0, row=0, sticky='W')
 mode_choice0 = tk.Radiobutton(mode_frame, text="Transmit", variable=mode, value="Tx",
-                              command=select_mode, indicatoron=0, font=(None, 32), width=15).grid(column=0, row=1, columnspan=2, sticky='W')
+                              command=select_mode, indicatoron=0, font=(None, 30), width=15).grid(column=0, row=1, columnspan=2, sticky='W')
 mode_choice1 = tk.Radiobutton(mode_frame, text="Receive", variable=mode, value="Rx",
-                              command=select_mode, indicatoron=0, font=(None, 32), width=15).grid(column=2, row=1, columnspan=2, sticky='W')
+                              command=select_mode, indicatoron=0, font=(None, 30), width=15).grid(column=2, row=1, columnspan=2, sticky='W')
 
 start_button = tk.Radiobutton(mode_frame, text="Start", variable=enabled, value=True,
-                              command=start, indicatoron=0, font=(None, 32), width=15).grid(column=0, row=2, columnspan=2, sticky='W')
+                              command=start, indicatoron=0, font=(None, 30), width=15).grid(column=0, row=2, columnspan=2, sticky='W')
 stop_button = tk.Radiobutton(mode_frame, text="Stop", variable=enabled, value=False,
-                             command=stop, indicatoron=0, font=(None, 32), width=15).grid(column=2, row=2, columnspan=2, sticky='W')
+                             command=stop, indicatoron=0, font=(None, 30), width=15).grid(column=2, row=2, columnspan=2, sticky='W')
 
 mode_frame.grid(column=0, row=0, rowspan=3, columnspan=4, pady=10)
 
 # ================ Frequency Specification ==================
 frequency_frame = tk.Frame(root)
 
-frequency_label = tk.Label(frequency_frame, text="Frequency", font=(None, 32)).grid(column=0, row=0, columnspan=2, sticky='W')
-frequency_entry = tk.Entry(frequency_frame, textvariable=frequency, width=13, font=(None, 32), justify=tk.CENTER).grid(column=0, row=1, columnspan=2, sticky='W')
+frequency_label = tk.Label(frequency_frame, text="Frequency", font=(None, 30)).grid(column=0, row=0, columnspan=2, sticky='W')
+frequency_entry = tk.Entry(frequency_frame, textvariable=frequency, width=13, font=(None, 30), justify=tk.CENTER).grid(column=0, row=1, columnspan=2, sticky='W')
 
 
 unit_choice0 = tk.Radiobutton(frequency_frame, text="Hz", variable=unit, value="Hz",
-                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=2, row=1, sticky='W')
+                              command=select_unit, indicatoron=0, font=(None, 30), width=8).grid(column=2, row=1, sticky='W')
 unit_choice1 = tk.Radiobutton(frequency_frame, text="kHz", variable=unit, value="kHz",
-                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=3, row=1, sticky='W')
+                              command=select_unit, indicatoron=0, font=(None, 30), width=8).grid(column=3, row=1, sticky='W')
 unit_choice2 = tk.Radiobutton(frequency_frame, text="MHz", variable=unit, value="MHz",
-                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=4, row=1, sticky='W')
+                              command=select_unit, indicatoron=0, font=(None, 30), width=8).grid(column=4, row=1, sticky='W')
 unit_choice3 = tk.Radiobutton(frequency_frame, text="GHz", variable=unit, value="GHz",
-                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=5, row=1, sticky='W')
+                              command=select_unit, indicatoron=0, font=(None, 30), width=8).grid(column=5, row=1, sticky='W')
 
-frequency_scale = tk.Scale(frequency_frame, variable=frequency, from_=0, to=999, orient=tk.HORIZONTAL, width=40, length=1240, sliderlength=60, resolution=0.1, font=(None, 32)).grid(column=0, row=2, columnspan=6, sticky='W')
+frequency_scale = tk.Scale(frequency_frame, variable=frequency, from_=0, to=999, orient=tk.HORIZONTAL, width=40, length=1240, sliderlength=60, resolution=0.1, font=(None, 30)).grid(column=0, row=2, columnspan=6, sticky='W')
 
 frequency_frame.grid(column=0, row=3, rowspan=2, columnspan=6, pady=10)
 
 # ================ Signal Type ==================
 radio_type_frame = tk.Frame(root)
 
-radio_type_label = tk.Label(radio_type_frame, text="Type", font=(None, 32)).grid(column=0, row=0, sticky='W')
+radio_type_label = tk.Label(radio_type_frame, text="Type", font=(None, 30)).grid(column=0, row=0, sticky='W')
 
 radio_type_values = (('AM', 'FM', 'USB'), ('LSB', 'CW', 'OTH'))
 
 for i, radio_type_row in enumerate(radio_type_values):
     for j, radio_type_value in enumerate(radio_type_row):
         radio_type_choice = tk.Radiobutton(radio_type_frame, text=radio_type_value, variable=radio_type, value=radio_type_value,
-                                           command=select_radio_type, indicatoron=0, font=(None, 32), width=15).grid(column=j*2, row=i+1, columnspan=2, sticky='W')
+                                           command=select_radio_type, indicatoron=0, font=(None, 30), width=15).grid(column=j*2, row=i+1, columnspan=2, sticky='W')
 
 radio_type_frame.grid(column=0, row=6, rowspan=3, columnspan=6, pady=10)
 
