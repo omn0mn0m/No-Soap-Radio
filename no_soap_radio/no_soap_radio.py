299018645cb5b322f21c14a0f3c3a66eb32f1dbe
Nam Tran <tranngocnam97@yahoo.com>
2018-04-28 14:38:15 -0400

    Added units

--- no_soap_radio/no_soap_radio.py
+++ no_soap_radio/no_soap_radio.py
@@ -16,25 +16,28 @@ mode.set('Rx')
 enabled.set(False)
 radio_type.set('FM')
 frequency.set(0)
+unit.set('Hz')
 
 metric_conversions = {'Hz':1e0, 'kHz': 1e3, 'MHz':1e6, 'GHz':1e9}
 
 def start():
     global p
     global p2
+
+    stop()
     
     if mode.get() == "Tx":
         p = sp.Popen(["ping", "www.google.com"])
         
-        print "Starting transmit at " + str(frequency.get())
+        print "Starting transmit at " + str(frequency.get() * metric_conversions[unit.get()])
     else:
         if radio_type.get() == 'FM':
-            p = sp.Popen(["rtl_fm", "-f", str(frequency.get()), "-M", "wbfm", "-s", "200000", "-r", "48000", "-"], stdout=sp.PIPE)
+            p = sp.Popen(["rtl_fm", "-f", str(frequency.get() * metric_conversions[unit.get()]), "-M", "wbfm", "-s", "200000", "-r", "48000", "-"], stdout=sp.PIPE)
             p2 = sp.Popen(["aplay", "-r", "48000", "-f", "S16_LE"], stdin=p.stdout)
         else:
             p = sp.Popen(["ping", "www.bing.com"])
         
-        print "Starting receive at " + str(frequency.get())
+        print "Starting receive at " + str(frequency.get() * metric_conversions[unit.get()])
 
 def stop():
     global p
@@ -43,19 +46,19 @@ def stop():
     if mode.get() == "Tx":
         try:
             p.terminate()
+
+            print "Stopping transmit at " + str(frequency.get() * metric_conversions[unit.get()])
         except:
             print "No process to close..."
-        
-        print "Stopping transmit at " + str(frequency.get())
     else:
         try:
             p.terminate()
             p2.terminate()
+            
+            print "Stopping receive at " + str(frequency.get() * metric_conversions[unit.get()])
         except:
             print "No process to close..."
         
-        print "Stopping receive at " + str(frequency.get())
-
 def select_mode(event=None):
     enabled.set(False)
     
@@ -70,6 +73,13 @@ def select_radio_type(event=None):
     
     print radio_type.get()
 
+def select_unit(event=None):
+    enabled.set(False)
+
+    stop()
+
+    print unit.get()
+
 def cleanup():
     stop()
 
@@ -82,20 +92,32 @@ mode_choice0 = tk.Radiobutton(mode_frame, text="Transmit", variable=mode, value=
 mode_choice1 = tk.Radiobutton(mode_frame, text="Receive", variable=mode, value="Rx",
                               command=select_mode, indicatoron=0, font=(None, 32), width=15).grid(column=2, row=1, columnspan=2, sticky='W')
 
-mode_frame.grid(column=0, row=0, rowspan=2, columnspan=4, pady=10)
+start_button = tk.Radiobutton(mode_frame, text="Start", variable=enabled, value=True,
+                              command=start, indicatoron=0, font=(None, 32), width=15).grid(column=0, row=2, columnspan=2, sticky='W')
+stop_button = tk.Radiobutton(mode_frame, text="Stop", variable=enabled, value=False,
+                             command=stop, indicatoron=0, font=(None, 32), width=15).grid(column=2, row=2, columnspan=2, sticky='W')
+
+mode_frame.grid(column=0, row=0, rowspan=3, columnspan=4, pady=10)
 
 # ================ Frequency Specification ==================
 frequency_frame = tk.Frame(root)
 
 frequency_label = tk.Label(frequency_frame, text="Frequency", font=(None, 32)).grid(column=0, row=0, columnspan=2, sticky='W')
-frequency_entry = tk.Entry(frequency_frame, textvariable=frequency, width=15, font=(None, 32), justify=tk.CENTER).grid(column=0, row=1, columnspan=2, sticky='W')
-start_button = tk.Radiobutton(frequency_frame, text="Start", variable=enabled, value=True,
-                              command=start, indicatoron=0, font=(None, 32), width=15).grid(column=2, row=1, columnspan=2, sticky='W')
-stop_button = tk.Radiobutton(frequency_frame, text="Stop", variable=enabled, value=False,
-                             command=stop, indicatoron=0, font=(None, 32), width=15).grid(column=4, row=1, columnspan=2, sticky='W')
-frequency_scale = tk.Scale(frequency_frame, variable=frequency, from_=0, to=1000000000, orient=tk.HORIZONTAL, width=40, length=1152, sliderlength=60, font=(None, 32)).grid(column=0, row=2, columnspan=6, sticky='W')
+frequency_entry = tk.Entry(frequency_frame, textvariable=frequency, width=13, font=(None, 32), justify=tk.CENTER).grid(column=0, row=1, columnspan=2, sticky='W')
 
-frequency_frame.grid(column=0, row=2, rowspan=2, columnspan=6, pady=10)
+
+unit_choice0 = tk.Radiobutton(frequency_frame, text="Hz", variable=unit, value="Hz",
+                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=2, row=1, sticky='W')
+unit_choice1 = tk.Radiobutton(frequency_frame, text="kHz", variable=unit, value="kHz",
+                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=3, row=1, sticky='W')
+unit_choice2 = tk.Radiobutton(frequency_frame, text="MHz", variable=unit, value="MHz",
+                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=4, row=1, sticky='W')
+unit_choice3 = tk.Radiobutton(frequency_frame, text="GHz", variable=unit, value="GHz",
+                              command=select_unit, indicatoron=0, font=(None, 32), width=8).grid(column=5, row=1, sticky='W')
+
+frequency_scale = tk.Scale(frequency_frame, variable=frequency, from_=0, to=999, orient=tk.HORIZONTAL, width=40, length=1240, sliderlength=60, resolution=0.1, font=(None, 32)).grid(column=0, row=2, columnspan=6, sticky='W')
+
+frequency_frame.grid(column=0, row=3, rowspan=2, columnspan=6, pady=10)
 
 # ================ Signal Type ==================
 radio_type_frame = tk.Frame(root)
@@ -109,7 +131,7 @@ for i, radio_type_row in enumerate(radio_type_values):
         radio_type_choice = tk.Radiobutton(radio_type_frame, text=radio_type_value, variable=radio_type, value=radio_type_value,
                                            command=select_radio_type, indicatoron=0, font=(None, 32), width=15).grid(column=j*2, row=i+1, columnspan=2, sticky='W')
 
-radio_type_frame.grid(column=0, row=5, rowspan=3, columnspan=6, pady=10)
+radio_type_frame.grid(column=0, row=6, rowspan=3, columnspan=6, pady=10)
 
 # ================ Main Loop ==================
 
